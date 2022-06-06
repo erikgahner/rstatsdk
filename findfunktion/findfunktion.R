4ca5e608d5c01e9fcff47142425fdbde41a2ee1d
Erik Gahner Larsen <erikgahner@gmail.com>
2022-06-06 16:23:03 +0200

    opdater tabel

--- findfunktion/findfunktion.R
+++ findfunktion/findfunktion.R
@@ -10,8 +10,13 @@ funktioner_data <- read_xlsx("funktioner.xlsx")
 funktioner_tabel <- funktioner_data |> 
   arrange(Pakke) |> 
   reactable(filterable = TRUE,
+            columns = list(
+              Beskrivelse = colDef(minWidth = 120),
+              Pakke = colDef(maxWidth = 120, style = list(fontFamily = "Courier, monospace")),
+              Funktion = colDef(maxWidth = 140)
+            ),
             style = list(fontFamily = "Work Sans, sans-serif", fontSize = "14px"),
-          defaultPageSize = 50) 
+            defaultPageSize = 50) 
 
 funktioner_tabel |> 
   save_reactable_test("funktioner_tabel.html")
